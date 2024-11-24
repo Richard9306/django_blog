5062b808052ddccd930b01eededa07ea49e9c752
Richard9306 <113300542+Richard9306@users.noreply.github.com>
2024-11-24 21:37:37 +0100

    add views

--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
 from django.views.generic import ListView
-from .models import Post
+from models import Post
 
 class BlogIndexView(ListView):
     model = Post
-    template_name = "blog/index.html"
+    template_name = "index.html"
     context_object_name = "posts"
     ordering = ["-created_on"]
 
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = "category.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        category = self.kwargs["category"]
+        return Post.objects.filter(categories__name__contains=category).order_by("-created_on")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        category = self.kwargs["category"]
+        context["category"] = category
+        return context
 
 
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from tkinter.font import names
+
 from django.contrib import admin
 from django.urls import path
+from django_blog.core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.BlogIndexView.as_view(), name="blog_index"),
 ]
