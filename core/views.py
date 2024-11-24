6a390d889fb9e8defcbffaa801ff40a2dc4dc76b
Richard9306 <113300542+Richard9306@users.noreply.github.com>
2024-11-24 16:20:33 +0100

    add BlogIndexView

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Post
+
+class BlogIndexView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-created_on"]
 
 
 
