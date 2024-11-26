68a664a2187d0ea87f6888f62326d3c162608262
Richard9306 <113300542+Richard9306@users.noreply.github.com>
2024-11-26 21:01:27 +0100

    improving managing of urls

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from tkinter.font import names
 
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.BlogIndexView.as_view(), name="blog_index"),
-    path("categories/<category>", views.CategoryListView.as_view(), name="categories"),
-    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail")
+    path("", include("core.urls")),
 ]
