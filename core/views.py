8d2da91c8d4011ad411423a573acd020c3ed37bd
Richard9306 <113300542+Richard9306@users.noreply.github.com>
2024-11-29 20:41:25 +0100

    add form for comments

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
+
+from .forms import CommentForm
 from .models import Post, Comment
 
 
@@ -31,6 +34,23 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(object_list=object_list, **kwargs)
-        context["comments"] = Comment.objects.filter(post=self.object).order_by('-created_on')
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        context["comments"] = Comment.objects.filter(post=post).order_by('-created_on')
+        context["form"] = CommentForm()
         return context
+
+    def post(self, requet, *args, **kwargs):
+        post = self.get_object()
+        form = CommentForm(requet.POST)
+
+        if form.is_valid():
+            Comment.objects.create(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            return HttpResponseRedirect(self.request.path_info)
+        context = self.get_context_data()
+        context["form"] = form
+        return self.render_to_response(context)
